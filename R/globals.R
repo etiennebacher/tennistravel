eea6f30f421c3584574b45e5014d204af7d1d83a
etiennebacher <etienne.bacher@protonmail.com>
2021-05-07 11:08:53 +0200

    fix last R CMD CHECK notes (except the one about size due to datasets)

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c("tennis_data", "arrival", "conversion_factors", "departure",
+                         "dist", "dist_per_tourn", "flightclass", "footprint",
+                         "footprint_per_tourn", "infos", "lat", "lat_a", "lat_d", 
+                         "long", "long_a", "long_d", "player_iso", "player_iso_2",
+                         "player_name", "tourney_location", "tourney_month",
+                         "tourney_year", "."))
